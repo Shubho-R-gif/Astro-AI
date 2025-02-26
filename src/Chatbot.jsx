fa050de878eaeaffd627b08a722f04ac51b10e2c
Shubho-R-gif <penguinr4@gmail.com>
2025-02-26 23:27:29 +0530

    Chatbot.jsx

--- src/Chatbot.jsx
+++ src/Chatbot.jsx
@@ -32,7 +32,8 @@ const Chatbot = () => {
   }, [transcript]);
 
   const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
-  const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
+  const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`
+;
 
   const handleSendBtn = async () => {
     if (userInputText.trim() === "") return;
